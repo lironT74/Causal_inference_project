78a91abb97768ef88556d173b0a031801134b55b
egor.markovsky <e1g9o9r9@gmail.com>
2021-03-09 10:48:44 +0200

    Objective, gradient

--- Estimators.py
+++ Estimators.py
@@ -1,7 +1,85 @@
 import numpy as np
+from scipy.optimize import minimize
+from numpy.linalg import norm
 
-def r_naive(delta, Y_hat, Obs):
-    return sum(delta(y_i, o_i) for y_i, o_i in zip(Y_hat, Obs)) / len(Obs)
+def params2vec(V, W, a, b, c):
+    V_new = V.flatten()
+    W_new = W.flatten()
+    a_new = a.flatten()
+    b_new = b.flatten()
+    c_new = c.flatten()
+    return np.concatenate([V_new, W_new, a_new, b_new, c_new])
 
-def ips(delta, y_hat, o, probs):
-    return sum(delta(y_i, o_i) for y_i, o_i in zip(Y_hat, Obs)) / len(Obs)
+
+def vec2params(params, num_of_users, num_of_items, inner_dim):
+    c = params[-1]
+    V = params[:num_of_users*inner_dim]
+    W = params[num_of_users*inner_dim:num_of_users*inner_dim+num_of_items*inner_dim]
+    remaining_params = params[num_of_users*inner_dim+num_of_items*inner_dim:-1]
+    a = remaining_params[:num_of_users]
+    b = remaining_params[num_of_users:]
+    return V.reshape(num_of_users, inner_dim), W.reshape(num_of_items, inner_dim), a, b, c
+
+def objective_gradient(params_vec, y_observed, propensities,
+                       num_of_users, num_of_items, inner_dim, lam, delta_type):
+    V, W, a, b, c = vec2params(params_vec, num_of_users, num_of_items, inner_dim)
+
+    # y_observed = np.ma.filled(y_observed, 0) # Make an assumption, that it is done outside
+    # propensities = np.ma.filled(propensities, 0) # The same
+    scores = np.matmul(V, W.T) + a + b + c
+    delta = y_observed-scores
+    if delta_type == 'MSE':
+        delta = delta ** 2
+    elif delta_type == 'MAE':
+        delta = np.abs(delta)
+    objective = (delta * propensities).sum()
+    regularization = lam*(norm(V)**2+norm(W)**2+norm(a)**2+norm(b)**2+c**2)
+    objective += regularization
+
+    if delta_type == 'MSE':
+        gradientMultiplier = propensities * 2 * delta
+    elif delta_type == 'MAE':
+        gradientMultiplier = np.zeros(delta.shape)
+        gradientMultiplier[delta > 0] = 1
+        gradientMultiplier[delta < 0] = -1
+        gradientMultiplier = propensities * gradientMultiplier
+    else:
+        raise
+
+    userVGradient = gradientMultiplier @ W
+    itemVGradient = gradientMultiplier.T @ V
+
+    userBGradient = gradientMultiplier.sum(1)
+    itemBGradient = gradientMultiplier.sum(0)
+    globalBGradient = gradientMultiplier.sum()
+
+    userVGradient += 2*lam*V
+    itemVGradient += 2*lam*W
+
+    userBGradient += 2*lam*a
+    itemBGradient += 2*lam*b
+    globalBGradient += 2*lam*c
+
+    gradient = params2vec(userVGradient, itemVGradient, userBGradient, itemBGradient, globalBGradient)
+
+    return objective, gradient
+
+def calculate_propencities(observed_ratings, inverse_propensities):
+    pass
+
+if __name__ == '__main__':
+    import scipy.sparse
+
+    rows = [2, 1, 4, 3, 0, 4, 3]
+    cols = [0, 2, 1, 1, 0, 0, 0]
+    vals = [1, 2, 3, 4, 5, 4, 5]
+    checkY = scipy.sparse.coo_matrix((vals, (rows, cols)))
+    checkY = checkY.toarray()
+    print(checkY)
+    checkY = np.ma.array(checkY, mask=checkY <= 0, hard_mask=True, copy=False)
+    print(checkY[checkY==5].count()/checkY.count())
+    print(checkY.size)
+    # randomPropensities = np.random.rand(Y_train.shape[0], Y_train.shape[1])
+    # print(randomPropensities)
+    # randomInvPropensities = np.reciprocal(randomPropensities)
+    # print(randomInvPropensities)
--- LoadData.py
+++ LoadData.py
@@ -13,17 +13,28 @@ def df_to_dict(df, first_key, second_key, value_key):
 
 def read_yahoo(path="data/yahoo_data"):
     column_names = ['user_id', 'song_id', 'rating']
-    df_train = pd.read_csv(path+"/ydata-ymusic-rating-study-v1_0-train.txt", '\t', names=column_names)
+    # df_train = pd.read_csv(path+"/ydata-ymusic-rating-study-v1_0-train.txt", '\t', names=column_names)
     df_test = pd.read_csv(path+"/ydata-ymusic-rating-study-v1_0-test.txt", '\t', names=column_names)
 
-    user2song_train = df_to_dict(df_train, 'user_id', 'song_id', 'rating')
-    user2song_test = df_to_dict(df_test, 'user_id', 'song_id', 'rating')
+    indexes = np.random.permutation(5400)
+    df_train_test = df_test.iloc[indexes[:int(5400*0.95)]]
+    df_test_test = df_test.iloc[indexes[int(5400*0.95):]]
 
-    with open(path+'/user2song_train.pkl', 'wb') as f:
-        pickle.dump(user2song_train, f)
+    # user2song_train = df_to_dict(df_train, 'user_id', 'song_id', 'rating')
+    user2song_train_test = df_to_dict(df_train_test, 'user_id', 'song_id', 'rating')
+    user2song_test_test = df_to_dict(df_test_test, 'user_id', 'song_id', 'rating')
 
-    with open(path+'/user2song_test.pkl', 'wb') as f:
-        pickle.dump(user2song_test, f)
+
+    # with open(path+'/user2song_train.pkl', 'wb') as f:
+    #     pickle.dump(user2song_train, f)
+
+    # with open(path+'/user2song_test.pkl', 'wb') as f:
+    #     pickle.dump(user2song_test, f)
+    with open(path+'/user2song_train_test.pkl', 'wb') as f:
+        pickle.dump(user2song_train_test, f)
+
+    with open(path+'/user2song_test_test.pkl', 'wb') as f:
+        pickle.dump(user2song_test_test, f)
 
 
 def load_yahoo(path="data/yahoo_data"):
@@ -36,17 +47,56 @@ def load_yahoo(path="data/yahoo_data"):
             song_id, raiting = list(song_raiting_pair.items())[0]
             Y_train[user_id-1, song_id-1] = raiting
 
-    with open(path+'/user2song_test.pkl', 'rb') as f:
-        user2song_test = pickle.load(f)
+    # with open(path+'/user2song_test.pkl', 'rb') as f:
+    #     user2song_test = pickle.load(f)
+    #
+    # Y_test = np.zeros((5400, 1000))
+    #
+    # for user_id in user2song_test:
+    #     for song_raiting_pair in user2song_test[user_id]:
+    #         song_id, raiting = list(song_raiting_pair.items())[0]
+    #         Y_test[user_id - 1, song_id - 1] = raiting
 
-    Y_test = np.zeros((5400, 1000))
+    with open(path+'/user2song_train_test.pkl', 'rb') as f:
+        user2song_train_test = pickle.load(f)
 
-    for user_id in user2song_test:
-        for song_raiting_pair in user2song_test[user_id]:
+    Y_train_test = dok_matrix((5400, 1000), dtype=np.float32)
+
+    for user_id in user2song_train_test:
+        for song_raiting_pair in user2song_train_test[user_id]:
             song_id, raiting = list(song_raiting_pair.items())[0]
-            Y_test[user_id - 1, song_id - 1] = raiting
+            Y_train_test[user_id - 1, song_id - 1] = raiting
 
-    return user2song_train, user2song_test, Y_train, Y_test
+    with open(path+'/user2song_test_test.pkl', 'rb') as f:
+        user2song_test_test = pickle.load(f)
+
+    Y_test_test = dok_matrix((5400, 1000), dtype=np.float32)
+
+    for user_id in user2song_test_test:
+        for song_raiting_pair in user2song_test_test[user_id]:
+            song_id, raiting = list(song_raiting_pair.items())[0]
+            Y_test_test[user_id - 1, song_id - 1] = raiting
+    # print(user2song_train)
+    # return user2song_train, user2song_test, Y_train, Y_test
+    return user2song_train, user2song_train_test, user2song_test_test, Y_train, Y_train_test, Y_test_test
 
 if __name__ == '__main__':
-    load_yahoo()
+    # read_yahoo()
+    user2song_train, user2song_train_test, user2song_test_test, Y_train, Y_train_test, Y_test_test = load_yahoo()
+
+    Y_train = Y_train.toarray()
+    Y_train = np.ma.array(Y_train, mask=Y_train <= 0, hard_mask=True, copy=False)
+
+    # print(Y_train)
+    Y_train_test = Y_train_test.toarray()
+    Y_train_test = np.ma.array(Y_train_test, mask=Y_train_test <= 0, hard_mask=True, copy=False)
+    print(np.ma.filled(Y_train_test, 0).mean())
+    print(Y_train_test.mean())
+    prob_obs = Y_train.count() / Y_train.size     # P(O=1)
+    p_y_o_list = {}
+    p_y_r = {}
+    for r in range(1, 6):
+        p_y_r_given_obs = Y_train[Y_train == r].count() / Y_train.count()    # P(Y=r|O=1)
+        p_y_o_list[r] = p_y_r_given_obs
+        p_y_r[r] = Y_train_test[Y_train_test == r].count() / Y_train_test.count()
+
