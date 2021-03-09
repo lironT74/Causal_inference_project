550f919fe0cb5644ecfd82d6baf0ac740bab665f
egor.markovsky <e1g9o9r9@gmail.com>
2021-03-09 12:49:19 +0200

    Strange learning

--- Estimators.py
+++ Estimators.py
@@ -1,6 +1,8 @@
 import numpy as np
 from scipy.optimize import minimize
 from numpy.linalg import norm
+from LoadData import load_yahoo
+
 
 def params2vec(V, W, a, b, c):
     V_new = V.flatten()
@@ -20,21 +22,20 @@ def vec2params(params, num_of_users, num_of_items, inner_dim):
     b = remaining_params[num_of_users:]
     return V.reshape(num_of_users, inner_dim), W.reshape(num_of_items, inner_dim), a, b, c
 
+
 def objective_gradient(params_vec, y_observed, propensities,
                        num_of_users, num_of_items, inner_dim, lam, delta_type):
     V, W, a, b, c = vec2params(params_vec, num_of_users, num_of_items, inner_dim)
 
-    # y_observed = np.ma.filled(y_observed, 0) # Make an assumption, that it is done outside
-    # propensities = np.ma.filled(propensities, 0) # The same
-    scores = np.matmul(V, W.T) + a + b + c
+    scale = num_of_users * num_of_items
+    scores = np.matmul(V, W.T) + np.array([a]).T + b + c
     delta = y_observed-scores
     if delta_type == 'MSE':
         delta = delta ** 2
     elif delta_type == 'MAE':
         delta = np.abs(delta)
     objective = (delta * propensities).sum()
-    regularization = lam*(norm(V)**2+norm(W)**2+norm(a)**2+norm(b)**2+c**2)
-    objective += regularization
+
 
     if delta_type == 'MSE':
         gradientMultiplier = propensities * 2 * delta
@@ -53,33 +54,86 @@ def objective_gradient(params_vec, y_observed, propensities,
     itemBGradient = gradientMultiplier.sum(0)
     globalBGradient = gradientMultiplier.sum()
 
-    userVGradient += 2*lam*V
-    itemVGradient += 2*lam*W
+    scaledPenalty = 1.0 * lam * scale / (num_of_users + num_of_items)
+    scaledPenalty /= (inner_dim + 1)
 
-    userBGradient += 2*lam*a
-    itemBGradient += 2*lam*b
-    globalBGradient += 2*lam*c
+    regularization = lam*(norm(V)**2+norm(W)**2+norm(a)**2+norm(b)**2+c**2)
+    objective += regularization
+
+    userVGradient += 2*scaledPenalty*V
+    itemVGradient += 2*scaledPenalty*W
+
+    userBGradient += 2*scaledPenalty*a
+    itemBGradient += 2*scaledPenalty*b
+    globalBGradient += 2*scaledPenalty*c
 
     gradient = params2vec(userVGradient, itemVGradient, userBGradient, itemBGradient, globalBGradient)
+    print(f'Objective: {objective}')
+    print(f'Gradient norm: {norm(gradient)}')
 
     return objective, gradient
 
-def calculate_propencities(observed_ratings, inverse_propensities):
-    pass
+
+def train_mf(y_observed, propensities, delta_type, inner_dim=1000, lam=1):
+    num_of_users, num_of_items = y_observed.shape
+    inverse_propensities = 1 / propensities
+
+    y_observed = np.ma.filled(y_observed, 0)
+    inverse_propensities = np.ma.filled(inverse_propensities, 0)
+
+    V = np.random.randn(num_of_users, inner_dim)
+    W = np.random.randn(num_of_items, inner_dim)
+    a = np.zeros(num_of_users, dtype=np.longdouble)
+    b = np.zeros(num_of_items, dtype=np.longdouble)
+    c = np.zeros(1)
+
+    x0 = params2vec(V, W, a, b, c)
+
+    x = minimize(objective_gradient, x0=x0, jac=True, method='L-BFGS-B', args=(y_observed, inverse_propensities,
+                                                                               num_of_users, num_of_items, inner_dim, lam, delta_type))
+
+    return x
+
+
+def get_observed_and_inverse_yahoo():
+    user2song_train, user2song_train_test, user2song_test_test, Y_train, Y_train_test, Y_test_test = load_yahoo()
+
+    Y_train = Y_train.toarray()
+    Y_train = np.ma.array(Y_train, mask=Y_train <= 0, hard_mask=True, copy=False)
+
+    Y_train_test = Y_train_test.toarray()
+    Y_train_test = np.ma.array(Y_train_test, mask=Y_train_test <= 0, hard_mask=True, copy=False)
+
+    p_o = Y_train.count() / Y_train.size  # P(O=1)
+    p_y_o_list = {}
+
+    for r in range(1, 6):
+        p_y_o = Y_train[Y_train == r].count() / Y_train.count()
+        p_y_r = Y_train_test[Y_train_test == r].count() / Y_train_test.count()
+        p_y_o_list[r] = p_y_o * p_o / p_y_r
+
+    propensities = Y_train.copy()
+    for r in range(1, 6):
+        propensities[propensities == r] = p_y_o_list[r]
+    return Y_train, Y_test_test, propensities
+
+
 
 if __name__ == '__main__':
-    import scipy.sparse
+    Y_train, Y_test_test, propensities = get_observed_and_inverse_yahoo()
 
-    rows = [2, 1, 4, 3, 0, 4, 3]
-    cols = [0, 2, 1, 1, 0, 0, 0]
-    vals = [1, 2, 3, 4, 5, 4, 5]
-    checkY = scipy.sparse.coo_matrix((vals, (rows, cols)))
-    checkY = checkY.toarray()
-    print(checkY)
-    checkY = np.ma.array(checkY, mask=checkY <= 0, hard_mask=True, copy=False)
-    print(checkY[checkY==5].count()/checkY.count())
-    print(checkY.size)
-    # randomPropensities = np.random.rand(Y_train.shape[0], Y_train.shape[1])
-    # print(randomPropensities)
-    # randomInvPropensities = np.reciprocal(randomPropensities)
-    # print(randomInvPropensities)
+    x = train_mf(Y_train, propensities, 'MSE', inner_dim=5, lam=1)
+
+    # import scipy.sparse
+    #
+    # rows = [2, 1, 4, 3, 0, 4, 3]
+    # cols = [0, 2, 1, 1, 0, 0, 0]
+    # vals = [1, 2, 3, 4, 5, 4, 5]
+    # checkY = scipy.sparse.coo_matrix((vals, (rows, cols)))
+    # checkY = checkY.toarray()
+    # print(checkY)
+    # checkY = np.ma.array(checkY, mask=checkY <= 0, hard_mask=True, copy=False)
+    # print(checkY[checkY==5].count()/checkY.count())
+    # print(checkY)
+    # print(checkY.count())
+    # print(checkY.size)
